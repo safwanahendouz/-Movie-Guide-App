edabb6e680e13d4cf96f598079a26c866187c8ec
safwanahendouz <safwanahendouz241@gmail.com>
2026-03-22 14:36:03 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,30 +6,50 @@ let result = document.getElementById("result");
 // Function to fetch data from API
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&
-    apikey=${key}`;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
     // If input field is empty
     if (movieName.length <= 0) {
         result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
-
     }
     // If input field is Not empty
     else {
-        fetch (url)
+        fetch(url)
             .then((resp) => resp.json())
             .then((data) => {
-                console.log(data);
-                console.log(data.Poster);
-                console.log(data.Title);
-                console.log(data.imbdRating);
-                console.log(data.Rated);
-                console.log(data.Year);
-                console.log(data.Runtime);
-                console.log(data.Genre);
-                console.log(data.Plot);
-                console.log(data.Actors);
+                if (data.Response === "True") {
+                    result.innerHTML = `
+                        <div class="info">
+                            <img src="${data.Poster}" class="poster" alt="${data.Title} poster">
+                            <div>
+                                <h2>${data.Title}</h2>
+                                <div class="rating">
+                                    <img src="star-icon.svg" alt="star">
+                                    <h4>${data.imdbRating}</h4>
+                                </div>
+                                <div class="details">
+                                    <span>${data.Rated}</span>
+                                    <span>${data.Year}</span>
+                                    <span>${data.Runtime}</span>
+                                </div>
+                                <div class="genre">
+                                    <div>${data.Genre.split(",").join("</div><div>")}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <h3>Plot:</h3>
+                        <p>${data.Plot}</p>
+                        <h3>Cast:</h3>
+                        <p>${data.Actors}</p>
+                    `;
+                } else {
+                    result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+                }
+            })
+            .catch(() => {
+                result.innerHTML = `<h3 class="msg">Error Occurred</h3>`;
             });
     }
 };
 
-window.addEventListener("load",getMovie);
+searchBtn.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
